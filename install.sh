ed22210911b2934d1c4206e699c291b7e356a721
moraesvic <talkto@moraesvic.com>
2024-08-03 11:03:43 -0300

    feat: make CLI more versatile through use of flags

--- install.sh
+++ install.sh
@@ -4,15 +4,8 @@ set -ex
 
 go build .
 
-if [ ! -d ~/bin/.executables ] ; then
-    mkdir -p ~/bin/.executables
+if [ ! -d ~/bin/ ] ; then
+    mkdir -p ~/bin/
 fi
 
-cp ./shenme ~/bin/.executables/shenme
-
-cat <<EOF > ~/bin/shenme
-#!/usr/bin/env bash
-~/bin/.executables/shenme "\$@" 2>/dev/null
-EOF
-
-chmod +x ~/bin/shenme
+cp ./shenme ~/bin/shenme
--- main.go
+++ main.go
@@ -1,33 +1,75 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	"github.com/moraesvic/shenme/lib"
 	"github.com/moraesvic/shenme/types"
 )
 
-func CLI(i types.IDefinerString, input string) {
+var (
+	isHTML  *bool
+	isDebug *bool
+)
+
+func Usage() {
+	fmt.Printf("Usage: %s [OPTIONS] <chinese-word>\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+func init() {
+	flag.Usage = Usage
+
+	isHTML = flag.Bool("html", false, "display as HTML (default is text)")
+	isDebug = flag.Bool("debug", false, "enable debug mode")
+
+	flag.Parse()
+}
+
+func CLIText(i types.IDefinerString, input string) {
 	traditional := lib.Traditional(input)
 
 	wikiURL := lib.GetWikiURL(traditional)
-	fmt.Printf("Obtaining definitions for %s %s at %s\n", input, lib.Pinyin(input), wikiURL)
+	fmt.Fprintf(os.Stderr, "Obtaining definitions for %s %s at %s\n", input, lib.Pinyin(input), wikiURL)
 
 	definitions := i.Define(traditional)
 	fmt.Print(definitions.String())
 
 	if definitions.Length() == 0 {
-		fmt.Println("No definitions were found.")
+		fmt.Fprintf(os.Stderr, "No definitions were found.")
 	}
 }
 
+func CLIHTML(i types.IDefinerHTML, input string) {
+	traditional := lib.Traditional(input)
+
+	wikiURL := lib.GetWikiURL(traditional)
+	fmt.Fprintf(os.Stderr, "Obtaining definitions for %s %s at %s\n", input, lib.Pinyin(input), wikiURL)
+
+	definitions := i.DefineHTML(traditional)
+	fmt.Print(definitions.HTML())
+}
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s <chinese-word>\n", os.Args[0])
-		panic(0)
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
 	}
 
-	input := os.Args[1]
-	CLI(lib.Definer{}, input)
+	input := flag.Args()[0]
+	definer := lib.Definer{}
+
+	if !*isDebug {
+		log.SetOutput(io.Discard)
+	}
+
+	if *isHTML {
+		CLIHTML(definer, input)
+	} else {
+		CLIText(definer, input)
+	}
 }
